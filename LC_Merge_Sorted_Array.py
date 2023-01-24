2047f09ad6c911289222e717df993dfccab641d1
Saraj Sadanand <sadanandsaraj@gmail.com>
2023-01-24 08:38:07 +0530

    changes

--- LC_Merge_Sorted_Array.py
+++ LC_Merge_Sorted_Array.py
@@ -1,5 +1,5 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m, nums2, n):
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -15,10 +15,10 @@ class Solution:
             # insert at d the larger of the two values from nums1 and nums2
             if p1 >= 0 and nums1[p1] > nums2[p2]:                
                 nums1[d] = nums1[p1]
-                p1 -= 1
+                p1 = p1 - 1
             else:
                 nums1[d] = nums2[p2]
-                p2 -= 1
+                p2 = p2 - 1
 
 
 nums1 = [1,2,3,0,0,0]
