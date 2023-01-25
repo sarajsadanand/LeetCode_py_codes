1d1413b3dc05133d9270923b6b9e9551823c2ec4
Saraj Sadanand <sadanandsaraj@gmail.com>
2023-01-25 18:06:30 +0530

    new code

--- LC_Intersection of Two Arrays II.py
+++ LC_Intersection of Two Arrays II.py
@@ -18,3 +18,26 @@ nums2 = [7,3,4,6]
 
 obj = Solution()
 obj.intersect(nums1, nums2)
+
+# Second approach using pointers
+class Solution1:
+    def intersect(self, nums1, nums2):
+        nums1.sort()
+        nums2.sort()
+        p1 = p2 = 0
+        res = []
+
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] < nums2[p2]:
+                p1 += 1
+            elif nums2[p2] < nums1[p1]:
+                p2 += 1
+            else:
+                res.append(nums1[p1])
+                p1 += 1
+                p2 += 1
+
+        return res
+                    
+obj1 = Solution1()
+obj1.intersect(nums1, nums2)
